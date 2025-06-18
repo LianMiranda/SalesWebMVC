331470a4d863351b06abeee58efc1b195325e757
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-06-18 16:19:18 -0400

    FEAT: Foreign key not null

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SalesWebMVC.Services
 
         public void Save(Seller seller)
         {
+            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
