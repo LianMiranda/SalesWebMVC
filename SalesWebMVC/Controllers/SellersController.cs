bc1c0e414fca9ae515fab8322980b64c2831cf32
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-06-17 21:56:05 -0400

    FEAT: Sellers Controller

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
